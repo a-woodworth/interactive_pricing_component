c26daa8e33f8a71c8bd1aa9efb19b33dddf899e7
Amy Woodworth <woodworthcommunications@gmail.com>
2023-02-22 10:48:21 -0600

    Add function to return updated plan info on range input.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const trialOffer30Form = document.forms['30DayTrial'];
+const range = trialOffer30Form.elements.pageviews;
+const pages = document.querySelector('[data-js="pageviews"]');
+const cost = document.querySelector('[data-js="price"]');
+const ariaPages = document.querySelector('[data-js="aria-pageviews"]');
+const ariaCost = document.querySelector('[data-js="aria-price"]');
+const planTypeText = document.querySelector('[data-js="billing-plan-type"]');
+
 const pricingTable = {
   pageviews: [
     { description: '10k', price: 8, rangeValue: 0 },
@@ -18,8 +26,36 @@ function numberToCurrency(amount) {
   return numberFormat.format(amount);
 }
 
+// Update price if annual plan selected
 function calculateDiscount(price, percentage) {
   const discountTotal = Math.floor( (price * percentage) / 100 );
   const reducedPrice = numberToCurrency( price - discountTotal );
   return reducedPrice;
 } 
+
+// Update plan details on range input
+function renderPlan(inputValue){
+  const rangeInput = inputValue;
+
+  // Return range values to get plan info
+  const plan = pricingTable.pageviews.map(value => {
+    return value.rangeValue;
+  });
+  const rangeIndex = plan.indexOf(rangeInput);
+
+  // Update plan details - pageviews and price
+  const description = pricingTable.pageviews[rangeIndex].description;
+  const amount = pricingTable.pageviews[rangeIndex].price;
+
+  pages.innerHTML = `${description}`;
+  ariaPages.innerHTML = `${description}`;
+  cost.innerHTML = `${numberToCurrency(amount)}`;
+  ariaCost.innerHTML = `${numberToCurrency(amount)}`;
+}
+
+range.addEventListener('change', () => {
+  const rangeVal = Number(trialOffer30Form.elements.pageviews.value);
+  renderPlan(rangeVal);
+});
+
+// trialOffer30Form.addEventListener('submit', validateForm);
